16f530c9af0abb2d13a6bb6b3aab29c6ee0fbf92
Sean Eric Thomas <set@seanericthomas.com>
2023-08-11 15:55:56 -0700

    Send json response for 404 and errors

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const indexRouter = require('./routes/index');
-
 const app = express();
 
 // set up mongoose
@@ -16,11 +14,31 @@ async function connectMongoDB() {
 }
 connectMongoDB().catch((err) => console.error(err));
 
+const json404 = (req, res, next) => {
+  res
+    .status(404)
+    .json({
+      status: 404,
+      message: `${req.url} not found for ${req.method} request`,
+    });
+};
+
+const jsonError = (err, req, res, next) => {
+  res.json({
+    status: res.statusCode,
+    message: err.message,
+  });
+};
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+// routes
+// XXX
+
+app.use(json404);
+app.use(jsonError);
 
 module.exports = app;
